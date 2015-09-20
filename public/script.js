6dcf9f2c1951d2c768c7f0b6d0611401313392ef
msmichellegar <msmichellegar@gmail.com>
2015-09-20 14:47:09 +0100

    mobile stuff and links

--- public/script.js
+++ public/script.js
@@ -1,3 +1,12 @@
 $(".hamburger").on('click', function() {
     $(".mobile-nav").slideToggle();
 });
+
+$(window).on('resize', function() {
+    if ($(window).width() < 900) {
+        $("a").removeClass('col-3');
+    }
+    if ($(window).width() > 900) {
+        $(".addclass").addClass('col-3');
+    }
+});
