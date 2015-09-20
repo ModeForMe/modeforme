25333bbfad3e6d33eb8084273caa10126dcd5bf4
msmichellegar <msmichellegar@gmail.com>
2015-09-20 16:30:36 +0100

    last minute fix

--- public/script.js
+++ public/script.js
@@ -10,3 +10,12 @@ $(window).on('resize', function() {
         $(".addclass").addClass('col-3');
     }
 });
+
+$(document).ready(function() {
+    if ($(window).width() < 900) {
+        $("a").removeClass('col-3');
+    }
+    if ($(window).width() > 900) {
+        $(".addclass").addClass('col-3');
+    }
+});
