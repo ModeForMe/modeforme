0c6091036b8094b7c1f55c7c2c68f2427ac52ce8
msmichellegar <msmichellegar@gmail.com>
2015-10-23 12:53:20 +0200

    removes error message from mc signup on homepage + progress bar function + updates package.json

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,10 @@
+// bringing out mobile nav menu
+
 $(".hamburger").on('click', function() {
     $(".mobile-nav").slideToggle();
 });
+
+// making third boxes mobile responsive
 
 $(window).on('resize', function() {
     if ($(window).width() < 900) {
@@ -19,3 +23,13 @@ $(document).ready(function() {
         $(".addclass").addClass('col-3');
     }
 });
+
+// progress bar demo
+
+function changeProgressBar () {
+    $(".progress-bar-one").replace(
+        "<img src='../static/public/images/progress-bar-two'>"
+    );
+};
+
+$(".join-waiting-list").click(changeProgressBar());
